7b5acb4fa6959c268dee3c27eec532ddac9a68f1
Tom Nguyen <tomnguyen@gmail.com>
2016-02-21 12:50:23 -0800

    Improved basic skeleton CSS/HTML.

--- themes/inforum/front-page.php
+++ themes/inforum/front-page.php
@@ -15,9 +15,10 @@ get_header(); ?>
 				<p>Inspired, unscripted conversations with leaders in politics, culture, tech, business, social issues, design, and more. <strong>Join us at Inforum.</strong></p>
 			</div>
 
-			<div class="cta section section-medium">
-				<p class="prompt">Be the first to know about incredible speakers and events coming to SF.</p>
-				<div class="temp-email">
+			<div class="section section-medium">
+				<div class="cta">
+					<p class="prompt">Be the first to know about incredible speakers and events coming to SF.</p>
+					<div class="temp-email"></div>
 				</div>
 			</div>
 
@@ -35,66 +36,81 @@ get_header(); ?>
 				</div>
 			</div>
 
-			<div class="center section section-medium">
+			<div class="section section-medium center">
 
-				<h2>What people are saying</h2>
-				<p>“INFORUM… has made a habit of programming insightful, celebrity-status speakers to discuss today’s critical issues”</p>
-				<p>San Francisco Magazine</p>
+				<div class="section-content width-medium">
+					<h2>What people are saying</h2>
 
-				<p>Recommended for singles looking for a “presidential forum and hang-out”</p>
-				<p>Los Angeles Times</p>
+					<p class="quote">“INFORUM… has made a habit of programming insightful, celebrity-status speakers to discuss today’s critical issues”</p>
+					<p class="attribution">San Francisco Magazine</p>
 
-				<p>“One of the city’s most treasured institutions”</p>
-				<p>San Francisco Chronicle</p>
+					<p class="quote">Recommended for singles looking for a “presidential forum and hang-out”</p>
+					<p class="attribution">Los Angeles Times</p>
 
+					<p class="quote">“One of the city’s most treasured institutions”</p>
+					<p class="attribution">San Francisco Chronicle</p>
+
+				</div>
 			</div>
 
-			<div class="speakers section section-light">
-				<div class="tag">Speakers</div>
+			<div class="section section-light">
 
+				<div class="speakers section-content">
+					<div class="speakers-primary">
+						<div class="tag">Speakers</div>
 
-				<div class="speakers-primary">
-					<h3>
-						Leaders of organizations, movements, and ideas that shape the world share their stories at Inforum.
-						<br/>
-						Every month, join us for a fun, illuminating, unforgettable evening.
-					</h3>
-				</div>
-
-				<div class="speakers-secondary">
-					<div class="recent-speakers">
-						<p class="intro">Some of our recent speakers:</p>
-						<p>Sal Khan</p>
-						<p>Marc Benioff</p>
-						<p>Sheryl Sandberg</p>
-						<p>Nate Silver</p>
-						<p>Jack Dorsey</p>
-						<p>Cornel West</p>
-						<p>Judy Blume</p>
-						<p>Mark Zuckerberg</p>
-						<p>Arianna Huffington</p>
-						<p>Reid Hoffman</p>
-						<p>James Franco</p>
-						<p>Megan Smith</p>
-						<p>David &amp; Tom Kelley </p>
-						<p>Rachel Maddow</p>
-						<p>Andy Cohen</p>
-						<p>Kim Kardashian West</p>
-						<p>Molly Ringwald</p>
-						<p>Talib Kweli</p>
-						<p>Adam Savage</p>
-						<p>Nicholas Kristof</p>
-						<p>Mario Batali</p>
-						<p>Alexis Ohanian</p>
-						<p>Jared Diamond</p>
-						<p>Cecile Richards</p>
-						<p>Marissa Mayer</p>
-						<p>Rashida Jones</p>
-						<p>John Maeda</p>
-						<p>Chuck Palahniuk</p>
+						<h3>
+							Leaders of organizations, movements, and ideas that shape the world share their stories at Inforum.
+						</h3>
+						<h3>
+							Every month, join us for a fun, illuminating, unforgettable evening.
+						</h3>
 					</div>
-				<div>
 
+					<div class="speakers-secondary">
+						<div class="recent-speakers">
+							<p class="intro">Some of our recent speakers:</p>
+							<p>Sal Khan</p>
+							<p>Marc Benioff</p>
+							<p>Sheryl Sandberg</p>
+							<p>Nate Silver</p>
+							<p>Jack Dorsey</p>
+							<p>Cornel West</p>
+							<p>Judy Blume</p>
+							<p>Mark Zuckerberg</p>
+							<p>Arianna Huffington</p>
+							<p>Reid Hoffman</p>
+							<p>James Franco</p>
+							<p>Megan Smith</p>
+							<p>David &amp; Tom Kelley </p>
+							<p>Rachel Maddow</p>
+							<p>Andy Cohen</p>
+							<p>Kim Kardashian West</p>
+							<p>Molly Ringwald</p>
+							<p>Talib Kweli</p>
+							<p>Adam Savage</p>
+							<p>Nicholas Kristof</p>
+							<p>Mario Batali</p>
+							<p>Alexis Ohanian</p>
+							<p>Jared Diamond</p>
+							<p>Cecile Richards</p>
+							<p>Marissa Mayer</p>
+							<p>Rashida Jones</p>
+							<p>John Maeda</p>
+							<p>Chuck Palahniuk</p>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="section section-dark center">
+				<div class="section-content width-medium">
+					<h2>Who’s next? Learn when amazing speakers come to SF</h2>
+					<div class="cta">
+						<p class="prompt">Don’t miss conversations that inspire you. Sign up to get the inside scoop about upcoming events in your inbox.</p>
+						<div class="temp-email"></div>
+					</div>
+				</div>
 			</div>
 
 
