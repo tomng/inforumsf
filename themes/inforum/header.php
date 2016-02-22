2033d81de25db7800a530cb8550297ff9ac8dbbd
Tom Nguyen <tomnguyen@gmail.com>
2016-02-22 01:02:54 -0800

    Fixed Source Sans Pro (added Google Fonts).

--- themes/inforum/header.php
+++ themes/inforum/header.php
@@ -14,6 +14,8 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300,400italic,600,700' rel='stylesheet' type='text/css'>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
